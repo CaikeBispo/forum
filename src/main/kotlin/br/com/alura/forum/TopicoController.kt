5307486f165c21c8955963d611d30aa5845dbe22
Caike Bispo <caikbispo@hotmail.com>
2023-02-23 22:58:12 -0300

    FEAT (TopicoController) Add register controller

--- src/main/kotlin/br/com/alura/forum/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/TopicoController.kt
@@ -1,26 +1,34 @@
 package br.com.alura.forum
 
+import br.com.alura.forum.dto.NovoTopicoDTO
 import br.com.alura.forum.model.*
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-@RequestMapping("/topico")
+@RequestMapping("/topics")
 class TopicoController(
     private val service: TopicoService
 ) {
     @GetMapping
-    fun list():List<Topico>{
+    fun list():List<Topic>{
         return service.list()
     }
 
     @GetMapping("/{id}")
-    fun listById(@PathVariable id: Long): Topico{
+    fun listById(@PathVariable id: Long): Topic{
         return service.listById(id);
+    }
+
+    @PostMapping
+    fun register(@RequestBody dto: NovoTopicoDTO) {
+        service.register(dto)
     }
 
 }
