7e654d9f46407c25b9442a3abe13d782929efd0f
Caike Bispo <caikbispo@hotmail.com>
2023-02-23 21:17:10 -0300

    FEAT(TopicoController) Extraindo para camada de serviço

--- src/main/kotlin/br/com/alura/forum/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/TopicoController.kt
@@ -1,37 +1,26 @@
 package br.com.alura.forum
 
 import br.com.alura.forum.model.*
+import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 import java.util.*
 
 @RestController
 @RequestMapping("/topico")
-class TopicoController {
+class TopicoController(
+    private val service: TopicoService
+) {
     @GetMapping
-    fun listar():List<Topico>{
-
-        var topico = Topico(
-            id = 1,
-            titulo = "Qualquer dúvida sobre SPRING",
-            mensagem = "Gostaria de qualquer resposta para minha pergunta",
-            datacriacao = LocalDateTime.now(),
-            curso = Curso(
-                1,
-                "Kotlin",
-                "Programação"
-            ),
-            autor = Usuario(
-                1,
-                "Caike Bispo",
-                "aa@aa.com"
-            ),
-            status = StatusTopico.NAO_RESPONDIDO,
-
-            )
-
-        return listOf(topico, topico, topico)
+    fun list():List<Topico>{
+        return service.list()
     }
+
+    @GetMapping("/{id}")
+    fun listById(@PathVariable id: Long): Topico{
+        return service.listById(id);
+    }
+
 }
