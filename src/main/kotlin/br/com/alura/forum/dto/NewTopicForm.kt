c7118ed5d1cc9cbb6c70026adeb69206aba62827
Caike Bispo <caikbispo@hotmail.com>
2023-04-16 16:06:53 -0300

    FEAT(TopicViewForm) Add Bean Validation on TopicViewForm

--- src/main/kotlin/br/com/alura/forum/dto/NewTopicForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/NewTopicForm.kt
@@ -1,8 +1,17 @@
 package br.com.alura.forum.dto
 
+//bean validation
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+
 data class NewTopicForm (
+    @field:NotEmpty @field:Size(min = 10, max = 100)
     val title: String,
+    @field:NotEmpty
     val message: String,
+    @field:NotNull
     val idCourse: Long,
+    @field:NotNull
     val idAuthor: Long,
 )
