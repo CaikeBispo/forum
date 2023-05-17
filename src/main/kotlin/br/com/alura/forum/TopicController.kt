c9f5e9460a2fc67c95fb51ee3aad43ab007d1bbf
Caike Bispo <caikbispo@hotmail.com>
2023-05-16 23:46:04 -0300

    Update TopicController.kt

--- src/main/kotlin/br/com/alura/forum/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/TopicController.kt
@@ -9,6 +9,7 @@ import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -23,7 +24,7 @@ class TopicController(
     @GetMapping
     fun list(
         @RequestParam(required = false) courseName:String?,
-        pagination: Pageable
+        @PageableDefault(size = 3) pagination: Pageable
     ):Page<TopicView>{
         return service.list(courseName, pagination)
     }
