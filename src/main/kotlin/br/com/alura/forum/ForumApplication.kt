ffd239310e843eef871c5acafdee9fd3cc88b6d3
Caike Bispo <caikbispo@hotmail.com>
2023-05-29 21:41:11 -0300

    Update ForumApplication.kt

--- src/main/kotlin/br/com/alura/forum/ForumApplication.kt
+++ src/main/kotlin/br/com/alura/forum/ForumApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableCaching
 class ForumApplication
 
 fun main(args: Array<String>) {
